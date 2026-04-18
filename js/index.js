4097b456febabdfb7920c395481f319841162839
gakurouo <gakurouo@gmail.com>
2026-04-18 12:50:10 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,12 @@
+clickimg = new Array()
+	clickimg[1] = new Image();clickimg[1].src ="pic/black_fish_02.gif";
+	clickimg[2] = new Image();clickimg[2].src ="pic/black_fish_08.gif";
+	clickimg[3] = new Image();clickimg[3].src ="pic/black_fish_04.gif";
+	clickimg[4] = new Image();clickimg[4].src ="pic/black_fish_08.gif";
+
+
+
+	var hiduke=new Date(document.lastModified);
+	var b=hiduke.getDate();
+	var c=hiduke.getMonth()+1;
+	var d=hiduke.getFullYear();
